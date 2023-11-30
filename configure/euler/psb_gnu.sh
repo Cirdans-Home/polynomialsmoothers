a3be9dd5ad7e83ea4e33dada8e6bae7f6aab48f0
cirdans-home <cirdan90@gmail.com>
2023-11-30 10:46:58 +0100

    Configure script for Euler workstation

--- configure/euler/psb_gnu.sh
+++ configure/euler/psb_gnu.sh
@@ -3,4 +3,4 @@
 	--with-metislibdir=/software/NUMERICAL/PARMETIS/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/ \
 	--with-ipk=4 --with-lpk=4 \
 	--with-amddir=/usr/include/suitesparse/ \
-	--prefix=/home/fdurastante/polynomialsmoothers/install 
+	--prefix=/home/fdurastante/polynomialsmoothers/install 	
