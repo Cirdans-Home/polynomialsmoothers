00978a441e4d6bc9b80b9687d6c0dd135aaffd13
Fabio Durastante <fabio.durastante@unipi.it>
2023-12-13 12:37:37 +0100

    Configure scripts for Karolina

---
+++ configure/karolina/environment.sh
@@ -0,0 +1,2 @@
+module load gcccuda/2022b gompi/2022b CMake/3.24.3-GCCcore-12.2.0 OpenBLAS/0.3.21-GCC-12.2.0
+module list
---
+++ configure/karolina/psb_gnu.sh
@@ -0,0 +1,9 @@
+./configure --prefix=/home/it4i-fdurast/polynomialsmoothers/install \
+	--with-metisincdir=/home/it4i-fdurast/polynomialsmoothers/install/include \
+	--with-metislibdir=/home/it4i-fdurast/polynomialsmoothers/install/lib \
+	--with-libs="-lGKlib -L/home/it4i-fdurast/polynomialsmoothers/install/lib"\
+	--with-ccopt="-O3 -fPIC" \
+	--with-fcopt="-O3 -frecursive" \
+	--with-cxxopt="-O3" \
+	--with-cuda=${CUDA_HOME} \
+
